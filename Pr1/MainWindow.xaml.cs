24c8fba966542f5f10b52afa3bfe65a3d5e457b0
toshakrugovov <isip_a.v.krugovovo@mpt.ru>
2024-04-01 14:58:54 +0300

    Добавьте файлы проекта.

---
+++ Pr1/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Pr1.employeeTableAdapters;
+
+namespace Pr1
+{
+
+ 
+    public partial class MainWindow : Window
+    {
+        // DataSet
+        EmployeeTableAdapter employee = new EmployeeTableAdapter();
+        Positions_of_employeesTableAdapter post = new Positions_of_employeesTableAdapter();
+
+        //EF
+
+        //employeeEntities context = new employeeEntities();
+
+
+        public MainWindow()
+        {
+            //DataSet
+            InitializeComponent();
+            EmployeeDgr.ItemsSource = employee.GetData();
+            pstCbx.ItemsSource = post.GetData();
+
+            //EF
+            //EmployeeDgr.ItemsSource = context.Employee.ToList(); //выгрузка
+            //pstCbx.ItemsSource = context.Positions_of_employees.ToList(); //выгрузка
+
+        }
+
+        /* private void EmployeeDgr_SelectionChanged(object sender, SelectionChangedEventArgs e)
+           {
+
+             //DataSet
+             var info = (EmployeeDgr.SelectedItem as DataRowView).Row;
+             empTbx.Text= info[1].ToString();//подтягивает данные из первого столбца
+             pstCbx.SelectedValue = Convert.ToInt32(info[4]); //подтягивает данные из привязки
+
+
+             //EF
+
+            //var info = (EmployeeDgr.SelectedItem as Employee);
+           //empTbx.Text = info.Surname; // подтягиваем данные из первого столбца
+             //pstCbx.SelectedItem= info.Positions_of_employees; // подтягиваем данные из привязки
+
+
+
+            } */
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+            {
+                Table2 table2 = new Table2();
+                table2.ShowDialog();
+               
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            employee.InsertQuery(NameTbx.Text,SurnameTbx.Text, PatronymicTbx.Text, Convert.ToInt32(pstCbx.SelectedValue));
+            EmployeeDgr.ItemsSource = employee.GetData();
+
+        }
+
+        private void Button_Click_0(object sender, RoutedEventArgs e)
+        {
+            if (EmployeeDgr.SelectedItem !=null)
+            {
+                var id = Convert.ToInt32((EmployeeDgr.SelectedItem as DataRowView).Row[0]);
+                employee.DeleteQuery(id);
+                EmployeeDgr.ItemsSource = employee.GetData();
+            }
+           
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (EmployeeDgr.SelectedItem != null)
+            {
+                var id = Convert.ToInt32((EmployeeDgr.SelectedItem as DataRowView).Row[0]);
+                employee.UpdateQuery(NameTbx.Text, SurnameTbx.Text, PatronymicTbx.Text, Convert.ToInt32(pstCbx.SelectedValue), id);
+                EmployeeDgr.ItemsSource = employee.GetData();
+            }
+        }
+    }
+}
+
---
+++ Pr1/Table2.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Pr1.employeeTableAdapters;
+
+namespace Pr1
+{
+    /// <summary>
+    /// Логика взаимодействия для Table2.xaml
+    /// </summary>
+    public partial class Table2 : Window
+    {
+
+        employeeEntities context = new employeeEntities();
+
+        public Table2()
+        {
+            InitializeComponent();
+            PostDrg.ItemsSource = context.Positions_of_employees.ToList();
+            empCbx.ItemsSource = context.Employee.ToList();
+
+        }
+
+       
+
+        private void PostDrg_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var info = PostDrg.SelectedItem as Positions_of_employees;
+            postTbx.Text = info.Post; // подтягиваем данные из первого столбца
+            empCbx.SelectedItem = info.Employee; // подтягиваем данные из привязки
+            employeeesalaryTbx.Text = info.Employee_salary.ToString();
+            
+
+
+
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+          var emp = new Pr1.Positions_of_employees();
+          emp.Post = postTbx.Text;
+          emp.Employee = null;
+          emp.Employee_salary = Convert.ToInt32(employeeesalaryTbx.Text);
+          context.Positions_of_employees.Add(emp);
+          context.SaveChanges();
+          PostDrg.ItemsSource= context.Positions_of_employees.ToList() ;
+
+
+
+        }
+
+        private void Button_Click_0(object sender, RoutedEventArgs e)
+        {
+            if(PostDrg.SelectedItem != null)
+            {
+                var emp = PostDrg.SelectedItem as Positions_of_employees;
+                context.Positions_of_employees.Remove(emp);
+                context.SaveChanges();
+                PostDrg.ItemsSource = context.Positions_of_employees.ToList();
+
+            }
+            
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (PostDrg.SelectedItem != null)
+            {
+                var emp = PostDrg.SelectedItem as Positions_of_employees;
+                emp.Post = postTbx.Text;
+                emp.Employee_salary = Convert.ToInt32(employeeesalaryTbx.Text);
+                context.SaveChanges();
+                PostDrg.ItemsSource = context.Positions_of_employees.ToList();
+
+            }
+            
+            
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainwindow = new MainWindow();
+            mainwindow.Show();
+            
+        }
+    }
+}
